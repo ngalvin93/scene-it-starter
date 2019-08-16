b73abd9921d3f65cf2a53e17cd41118a3da0a228
Alvin Ng <ngalvin93@gmail.com>
2019-08-16 08:32:01 -0500

    added css to movie cards and started on index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let renderMovies = function (movieArray) {
+        let movieHTML = movieArray.map(function (currentMovie) {
+            $('.movie-poster').
+        });
+    }
+});
