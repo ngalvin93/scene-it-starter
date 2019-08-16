e9cb04cfe6b1fa1a6918a90a78c263170db127d1
Alvin Ng <Alvinn@lovetthomes.com>
2019-08-16 17:24:20 -0500

    completed step 2

--- index.js
+++ index.js
@@ -9,8 +9,38 @@
 //     }
 // });
 
+// document.addEventListener('DOMContentLoaded', function () {
+//         function renderMovies (movieData) {
+//         $(movieData).map(function (currentMovie) {
+//             console.log(`
+//             <div class="movie card my-3 mx-3 py-3 px-3" style="width: 18rem;">
+//                 <img class="movie-poster card-img-top" src="${currentMovie.Poster}">
+//                 <div class="body">
+//                     <p class="card-title">${currentMovie.Title}</p>
+//                     <p class="card-text">${currentMovie.Year}</p>
+//                     <a href="#" class="btn btn-primary">Add Movie</a>
+//                 </div>
+//             </div>
+//             `)
+//         })
+// }
+
 document.addEventListener('DOMContentLoaded', function () {
-    function renderMovies () {
-        
+    function renderMovies (movieArray) { // what do the parameters in each function equal to?
+        var movieHTML = movieArray.map(function (currentMovie) {
+            return `
+            <div class="movie card my-3 mx-3 py-3 px-3" style="width: 18rem;">
+            <img class="movie-poster card-img-top" src="${currentMovie.Poster}">
+            <div class="body">
+            <p class="card-title">${currentMovie.Title}</p>
+            <p class="card-text">${currentMovie.Year}</p>
+            <a href="#" class="btn btn-primary">Add Movie</a>
+            </div>
+            </div>
+            `
+        })
+        return movieHTML.join('')
     }
+    var movieContainer = document.querySelector('.movies-container')
+    movieContainer.innerHTML = renderMovies(movieData)
 })
